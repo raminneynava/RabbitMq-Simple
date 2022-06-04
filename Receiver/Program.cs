0653006ad6a526f0eaf390314388238b6e0c3236
raminneynava <raminneynava@gmail.com>
2022-06-04 19:07:26 +0430

    Add project files.

---
+++ Receiver/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Receiver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.BasicQos(0,1,false);
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received+=(sender, args) => {
+
+              var body=  args.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(message);
+                channel.BasicAck(args.DeliveryTag,true);
+
+            };
+            channel.BasicConsume("myqueue01", true, consumer);
+            Console.ReadLine();
+           
+        }
+    }
+}
---
+++ Sender/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+using RabbitMQ.Client;
+
+namespace Sender
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.QueueDeclare("myqueue01", true, false, false, null);
+            for (int i = 1; i < 100; i++)
+            {
+                string message = $"This is test message for my sender at{DateTime.Now.Ticks}";
+                var body = Encoding.UTF8.GetBytes(message);
+                var peroperties = channel.CreateBasicProperties();
+                peroperties.Persistent = true; //save message in disk
+                channel.BasicPublish("", "myqueue01", peroperties, body);
+            }
+            channel.Close();
+            connection.Close();
+            Console.ReadLine();
+        }
+    }
+}
